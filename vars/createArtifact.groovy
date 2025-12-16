d4e6256dfafa12e63eecf711d795615b6edde788
ramtruely <ramtruely6@gmail.com>
2025-12-16 18:34:52 +0530

    Update createArtifact.groovy

--- vars/createArtifact.groovy
+++ vars/createArtifact.groovy
@@ -1,5 +1,20 @@
 def createArtifact(String name) {
-    println "Artifact created: ${name}"
+    def workspace = new File(".").canonicalPath
+    def buildDir  = new File("build")
+
+    if (!buildDir.exists()) {
+        buildDir.mkdirs()
+    }
+
+    def artifactFile = new File(buildDir, "${name}.txt")
+
+    artifactFile.text = """
+Artifact Name : ${name}
+Created At    : ${new Date()}
+Workspace     : ${workspace}
+""".stripIndent().trim()
+
+    println "Artifact created at: ${artifactFile.absolutePath}"
 }
 
 return this
