12de74969b92a2f391ed225bc9a480c9dc943721
ramtruely <ramtruely6@gmail.com>
2025-12-16 17:07:26 +0530

    Create Logger.groovy

---
+++ src/com/company/utils/Logger.groovy
@@ -0,0 +1,11 @@
+package com.company.utils
+
+class Logger {
+    static void info(String msg) {
+        println "[INFO] ${msg}"
+    }
+
+    static void error(String msg) {
+        println "[ERROR] ${msg}"
+    }
+}
